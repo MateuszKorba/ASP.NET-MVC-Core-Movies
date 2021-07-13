73967705297146101454c079a8b1753fce033f17
MateuszKorba <mkorba90@gmail.com>
2021-07-13 15:40:27 +0200

    Add new files

---
+++ WebAppi6/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppi6.Models;
+
+namespace WebAppi6.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options){}
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
